0198fe769f69f72958f943fe60253354464a09f2
aisyahwlandr <aisyahwlandr@gmail.com>
2024-05-14 13:45:25 +0700

    fix route google-login

--- ch5/topic-1/route/auth.js
+++ ch5/topic-1/route/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, profile } = require("../controller/auth");
+const { register, login, profile, googleLogin } = require("../controller/auth");
 const { authMiddleware } = require("../middleware/auth");
-const { default: GoogleLogin } = require("../../../ch6/topic-1/src/components/GoogleLogin");
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("google-login", GoogleLogin);
+router.post("google-login", googleLogin);
 router.get("/profile", authMiddleware(["user", "admin"]), profile);
 
 module.exports = router;
